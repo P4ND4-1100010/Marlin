e0358e4cd6ded3c0ff5bbbac2286e94f23d0342e
P4ND4-1100010 <javiercarrillo.main@gmail.com>
2022-01-16 18:41:36 +0100

    Calibration Z_PROBE_OFFSET

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1258,7 +1258,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 51, -7, -1.52 }
+#define NOZZLE_TO_PROBE_OFFSET { 51, -7, -2.91 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
