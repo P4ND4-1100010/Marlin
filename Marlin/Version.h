e1fe66a8e61ccccf65ed1de773bf489114615736
P4ND4-1100010 <javiercarrillo.main@gmail.com>
2022-01-18 00:03:30 +0100

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -29,7 +29,7 @@
  * Marlin release version identifier
  *
  */
-#define SHORT_BUILD_VERSION "2.0.9.3-N2-1.3"
+#define SHORT_BUILD_VERSION "2.0.9.3-N2-rc2"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -42,7 +42,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2022-01-07"
+#define STRING_DISTRIBUTION_DATE "2022-01-18"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
